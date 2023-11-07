b4781fba6e3378766188d45d27fe1354b3e51fda
Manu <141773224+ManuMan32@users.noreply.github.com>
2023-11-07 16:25:01 -0300

    Comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 "use strict";
 
+// Variables
 const canvas = document.getElementById("canvas");
 const dif = canvas.getBoundingClientRect();
 const ctx = canvas.getContext("2d");
 let painting = false;
 let color, width, difX, difY;
 
+// Start painting when the user clicks
 canvas.addEventListener("mousedown",e=>{
 	difX = e.clientX - dif.left;
 	difY = e.clientY - dif.top;
@@ -15,6 +17,7 @@ canvas.addEventListener("mousedown",e=>{
 	ctx.beginPath();
 });
 
+// Creates the line when the mouse moves
 canvas.addEventListener("mousemove",e=>{
 	if (painting) {
 		const newDifX = e.clientX - dif.left;
@@ -25,11 +28,13 @@ canvas.addEventListener("mousemove",e=>{
 	}
 });
 
+// Stop painting
 canvas.addEventListener("mouseup",e=>{
 	ctx.closePath();
 	painting = false;
 })
 
+// Function that draws the line from a (x1,y1) to a (x2,y2)
 function draw(x1,y1,x2,y2) {
 	ctx.strokeStyle = color;
 	ctx.lineWidth = width;
